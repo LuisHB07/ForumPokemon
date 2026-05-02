974b8b91263f2528325a2c973aea7c449950ba03
LuisHB07 <luis.barreira@sptech.school>
2026-05-02 20:21:10 -0300

    Adicionando o web-data-viz, finalizando o cadastro e login com API e banco de dados

---
+++ src/database/script-tabelas.sql
@@ -0,0 +1,31 @@
+-- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
+-- Você precisa executar os comandos no banco de dados para criar as tabelas,
+-- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+
+/*
+comandos para mysql server
+*/
+
+CREATE DATABASE PokeTeam;
+USE PokeTeam;
+
+CREATE TABLE usuario (
+	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45) NOT NULL,
+    email VARCHAR(45) NOT NULL,
+    senha VARCHAR(45) NOT NULL
+);
+CREATE TABLE equipe (
+	idEquipe INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45) NOT NULL,
+    fkUsuario INT,
+    CONSTRAINT fkUsuario FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario)
+);
+CREATE TABLE pokemon (
+	idPokemon INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45) NOT NULL,
+    tipo1 VARCHAR(45) NOT NULL,
+    tipo2 VARCHAR(45),
+    fkEquipe INT,
+    CONSTRAINT fkEquipe FOREIGN KEY (fkEquipe) REFERENCES equipe(idEquipe)
+);
