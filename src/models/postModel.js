8b3b82089b6161a0ff5d3ddffb1919c5f1402c3e
Luis Sptech <luis.barreira@sptech.school>
2026-05-25 19:27:44 -0300

    Ajustes no botão de deletar post

--- src/models/postModel.js
+++ src/models/postModel.js
@@ -2,7 +2,8 @@ var database = require("../database/config");
 
 function publicar(titulo, descricao, fkEquipe, idUsuario){
     var instrucaoSql = `
-        INSERT INTO post (titulo, descricao, fkUsuario, fkEquipe) VALUES ('${titulo}', '${descricao}', ${idUsuario}, ${fkEquipe});
+        INSERT INTO post (titulo, descricao, fkUsuario, fkEquipe) VALUES 
+        ('${titulo}', '${descricao}', ${idUsuario}, ${fkEquipe});
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
@@ -10,7 +11,8 @@ function publicar(titulo, descricao, fkEquipe, idUsuario){
 
 function listar(){
     var instrucaoSql = `
-    SELECT p.idPost, p.titulo, p.descricao, p.fkEquipe, u.nome AS nomeUsuario, e.nome AS nomeEquipe FROM post p JOIN usuario u ON p.fkUsuario = u.idUsuario JOIN equipe e ON p.fkEquipe = e.idEquipe;
+    SELECT p.idPost, p.titulo, p.descricao, p.fkEquipe, u.nome AS nomeUsuario, e.nome AS nomeEquipe, u.idUsuario AS idUsuario FROM post p 
+    JOIN usuario u ON p.fkUsuario = u.idUsuario JOIN equipe e ON p.fkEquipe = e.idEquipe;
     `
     console.log("Executando a instrução SQL: \n" + instrucaoSql)
     return database.executar(instrucaoSql)
